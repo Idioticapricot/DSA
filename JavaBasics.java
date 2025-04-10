d157930824fd4d29fe3dc30fb296330b296d6422
Idioticapricot <avkarthikeya123@gmail.com>
2025-04-10 23:00:46 +0530

    factorial

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,22 +1,17 @@
 
-import java.util.Scanner;
 
 public class JavaBasics{
-   public static int product(int a,int b){
-    int product = a*b;
-    return product ;
+   public static int factorial (int n){
+    int f = 1;
+    for(int i = 1;i<=n;i++){
+        f=f*i;
+
+    }
+    return f;
    }
     public static void main(String[] args) {
-       System.out.println("Enter A :");
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter A :");
-       int a = sc.nextInt();
-       System.out.println("Enter b :");
-       int b = sc.nextInt();
-       int prod = product(a, b);
-       System.out.println("Product  = "+prod);
-
-       
+      
+       System.out.println(factorial(15));
 
     }
 } 
