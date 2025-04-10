38eb1f7ad928b3242f3d2733d4745acd174db3cd
Saisiddhu09 <myselfsaisiddhu@gmail.com>
2025-04-10 21:41:07 +0530

    first commit

---
+++ JavaBasics.java
@@ -0,0 +1,10 @@
+public class JavaBasics{
+    public static void PrintHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        PrintHelloWorld();//function call
+    }
+}
