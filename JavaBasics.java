fd3d656331946e4436af867a473fd9c963d9208e
Idioticapricot <avkarthikeya123@gmail.com>
2025-04-10 22:01:02 +0530

    #

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,10 +1,24 @@
+import java.util.*;
 public class JavaBasics{
+    public static void calculateSum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A :");
+        int a = sc.nextInt();
+        System.out.print("Enter B :");
+        int b = sc.nextInt();
+ 
+        int sum = a+b;
+        System.out.println("The Sum is : "+ sum);
+        
+
+    }
     public static void PrintHelloWorld(){
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
     }
     public static void main(String[] args) {
-        PrintHelloWorld();//function call
+        //PrintHelloWorld();//function call
+        calculateSum();
     }
 }
