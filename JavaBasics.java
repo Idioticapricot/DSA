0392f2dfdd1f33544af90f5e95f6476c6ec6fc43
Idioticapricot <akarthikeyasmail@gmail.com>
2025-04-13 11:54:23 +0530

    pairs in array

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,28 +1,21 @@
 import java.util.*;
 public class JavaBasics{
-    public static void Bsearch(int arr[],int key)
-    {
-        int start = 0;
-        int end = arr.length-1;
-     
-     while(start<=end){
-        
-        int mid = start + (end - start) / 2;;
-        if(arr[mid] == key){
-            System.out.print("found at :" +mid);
+    public static void pairs(int nums[])
+
+ {
+    for(int i =0;i<nums.length;i++){
+        for(int n=i+1;n<nums.length;n++){
+            System.out.println("Pair: (" + nums[i] + ", " + nums[n] + ")");
         }
-        else if(arr[mid] > key){
-            end = mid -2;
-        }
-        else{start=mid+1;}
-        return;
-     }
     }
+   
+ }
+    
     public static void main(String[] args) 
     {
-        int arr[] ={2,4,6,8,10,12,14};
-        int key = 8;
-        Bsearch(arr, key);
+        int nums [] = {2,4,6,8,10};
+        pairs(nums);
+       
     }
     
 
