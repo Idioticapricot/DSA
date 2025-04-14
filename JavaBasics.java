0afc4922fe45611d5f798f5b15038c28d4952fba
Idioticapricot <akarthikeyasmail@gmail.com>
2025-04-14 23:38:25 +0530

    trapping rainwater

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,20 +1,43 @@
 import java.util.*;
 public class JavaBasics{
-    public static void maxsubarray(int nums[])
+    public static void rainwater(int height[])
 
  {
-    int cursum =0;
-    int maxsum = 0;
 
-    for (int i =0 ; i < nums.length ; i++){
+    
+    int trapped =0;
+    
+
+    for( int i = 0; i < height.length; i++){
+        int maxl = 0;
+        int maxr = 0;
+        int left =0;
+        int right =0;
+        int waterlevel =0;
         
-        cursum += nums [i];
-        if(cursum < 0) cursum = 0;
-        maxsum = Math.max(maxsum, cursum);
-       
-         
+        for( int l = i; l>= 0 ; l--){
+        left = height[l];
+        
+            if(maxl < left) maxl = left;
+        }
+        for (int r =i ; r < height.length; r++)
+        {
+        right  = height[r];
+        if(maxr < right) maxr = right;
+        }
+        int waterLevel = Math.min(maxl, maxr);
+            if (waterLevel > height[i]) {
+                trapped += waterLevel - height[i];
+}
+
+
+
+        
+        
     }
-    System.out.println("max sum is : " + maxsum);
+    
+    
+    
     
     
    
@@ -22,8 +45,9 @@ public class JavaBasics{
     
     public static void main(String[] args) 
     {
-        int nums [] = {-2,-3,4,-1,-2,1,5,-3};
-        maxsubarray(nums);
+        int height [] = {4,2,0,6,3,2,5};
+        rainwater(height);
+        
 
     }
     
