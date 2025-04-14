ded041ff37d886436679d4f1ebbbdfa91a53d548
Idioticapricot <akarthikeyasmail@gmail.com>
2025-04-14 12:36:35 +0530

    kadans algo

--- JavaBasics.java
+++ JavaBasics.java
@@ -3,39 +3,26 @@ public class JavaBasics{
     public static void maxsubarray(int nums[])
 
  {
-    int maxsum=Integer.MIN_VALUE;;
+    int cursum =0;
+    int maxsum = 0;
+
+    for (int i =0 ; i < nums.length ; i++){
+        
+        cursum += nums [i];
+        if(cursum < 0) cursum = 0;
+        maxsum = Math.max(maxsum, cursum);
+       
+         
+    }
+    System.out.println("max sum is : " + maxsum);
     
-    int prefix[]= new int[nums.length];
-    prefix[0]=nums[0];
-    for (int i=1;i<nums.length;i++){
-        prefix[i]=prefix[i-1]+nums[i];
-    }
-    for(int a=0;a<nums.length;a++){
-        int currsum =0;
-        int start = a;
-        for(int b=a;b<nums.length;b++){
-            int end = b;
-             currsum = start == 0 ? prefix[end]:   prefix[end] - prefix[start -1];
-
-             if(maxsum < currsum){
-                maxsum=currsum;
-             }
-            
-        }
-        
-        
-        
-
-
-    }
-    System.out.println("max sum =" + maxsum);
     
    
  }
     
     public static void main(String[] args) 
     {
-        int nums [] = {1,-2,6,-1,3};
+        int nums [] = {-2,-3,4,-1,-2,1,5,-3};
         maxsubarray(nums);
 
     }
